cd5a3c1b02672e182fc3fe65cb3f9777b42a529f
Nick xu <xutan@hddomain.cn>
2016-09-29 14:52:34 +0800

    扫码完成了 20%

--- gitFlow_test/gitFlow_test/AppDelegate.swift
+++ gitFlow_test/gitFlow_test/AppDelegate.swift
@@ -25,8 +25,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        /*
+         扫码完成了 20%
+         */
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
